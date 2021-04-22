60c9e408b163c1968470844006bbad54ddf39a37
Deval <deval.italiya@gmail.com>
2021-04-22 18:43:42 +0530

    22-4 deval

--- app/src/main/java/com/example/familytracker/MainActivity.java
+++ app/src/main/java/com/example/familytracker/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.familytracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.familytracker.fragment.ChatFragment;
+import com.example.familytracker.fragment.ExploreFragment;
+import com.example.familytracker.fragment.FriendsFragment;
+import com.example.familytracker.fragment.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    private ExploreFragment exploreFragment;
+    private FriendsFragment friendsFragment;
+    private ChatFragment chatFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.main_frame);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        exploreFragment = new ExploreFragment();
+        friendsFragment = new FriendsFragment();
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        setFragment(exploreFragment);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_explore:
+                        setFragment(exploreFragment);
+                        return true;
+
+                    case R.id.nav_friends:
+                        setFragment(friendsFragment);
+                        return true;
+
+                    case R.id.nav_chat:
+                        setFragment(chatFragment);
+                        return true;
+
+                    case R.id.nav_profile:
+                        setFragment(profileFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+
+
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
     }
 }
